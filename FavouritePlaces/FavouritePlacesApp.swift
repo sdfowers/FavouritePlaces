7f3cf40c4bff0d99e8170db21b0be8d9051d0d8d
Sean Fowers <sean.fowers@griffithuni.edu.au>
2023-04-26 23:41:02 +1000

    Initial Commit

---
+++ FavouritePlaces/FavouritePlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouritePlacesApp.swift
+//  FavouritePlaces
+//
+//  Created by Sean Fowers on 26/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct FavouritePlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
